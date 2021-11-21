11e254dad5f983cf6d701c00b1c80b2bc05e3fd2
Alina923 <46137501+Alina923@users.noreply.github.com>
2021-11-21 20:32:33 +0300

    Add files via upload

---
+++ lab_4/server/src/main/java/com/zuul/gateway/controller/DataObjectController.java
@@ -0,0 +1,30 @@
+package com.zuul.gateway.controller;
+
+import com.zuul.gateway.domain.DataObject;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class DataObjectController {
+    @RequestMapping(value = "/data/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public DataObject findById(@PathVariable long id, HttpServletRequest req, HttpServletResponse res) {
+        if (req.getHeader("Test") != null) {
+            res.addHeader("Test", req.getHeader("Test"));
+        }
+        return new DataObject(id, "121");
+    }
+
+
+}
+/*@RestController
+public class DataObjectController {
+
+    @RequestMapping(value = "/data/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public DataObject findById(@PathVariable long id) {
+        return new DataObject(id,"345");
+    }
+}*/
